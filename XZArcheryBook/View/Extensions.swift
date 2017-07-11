3b03d65cf9034bd1bbf38c0b655b9a5d28e0a503
1529007607 <1529007607@qq.com>
2017-07-11 18:33:28 +0800

    Add Archery Target View

---
+++ XZArcheryBook/View/Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  Extensions.swift
+//  XZArcheryBook
+//
+//  Created by Andrew Chai on 7/11/17.
+//  Copyright © 2017 祥子. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+}
